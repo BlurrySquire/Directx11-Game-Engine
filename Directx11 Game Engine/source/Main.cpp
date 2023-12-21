dc7a6107fc245de07ff2718937a11efe7643e421
Riley Hawksworth <rileyrooh13@gmail.com>
2023-12-21 20:26:28 +0000

    Basic Error Logging using WIndows Message Boxes.

--- Directx11 Game Engine/source/Main.cpp
+++ Directx11 Game Engine/source/Main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+
+#include <Windows.h>
+#include <comdef.h>
+
+static std::wstring WideString(std::string string)
+{
+	return std::wstring(string.begin(), string.end());
+}
+
+static void LogError(HRESULT hResult, std::wstring errorMessage)
+{
+	_com_error error(hResult);
+	std::wstring error_message = errorMessage + L"\n\n" + error.ErrorMessage();
+	MessageBoxW(NULL, error_message.c_str(), L"Game Engine Error", MB_ICONERROR);
+}
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello, World!" << std::endl;
+	LogError(E_ABORT, WideString("Hello, World!"));
 	return 0;
 }
